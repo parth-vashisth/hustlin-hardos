9727c08d435cebdc9d7fcd3a155b34d5b2661d96
parth vashisth <shrmaparth123@gmail.com>
2024-06-27 16:06:29 +0530

    slider complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,33 @@ $('.parentslider').slick({
         }
     ]
 });
+$('.parentslide').slick({
+    dots: false,
+    infinite: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
 const btnsub = document.querySelector(".sub-btn");
 const btnadd = document.querySelector(".add-btn");
 const number = document.querySelector(".number");
